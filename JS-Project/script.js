f6cfeee50459567271177ac9bb35328991329137
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-03-03 17:04:14 +0530

    JS-Project done

--- JS-Project/script.js
+++ JS-Project/script.js
@@ -1,25 +1,20 @@
 const API = "https://65e294dca8583365b31844ea.mockapi.io/student";
-//CRUD - CREATE READ UPDATE DELETE
-const dummyData = {
-  name: "new name",
-  batch: "new batch",
-  age: 65,
-  id: "6",
-};
 
-const changeName = {
-  name: "name 6",
-};
+const studContainer = document.getElementById("student-data");
+const studentForm = document.getElementById("student-form");
+let editId;
+//CRUD - CREATE READ UPDATE DELETE
+
 //helper function
 //Get API   // READ
-
 async function getStudentsData(id) {
   try {
     const res = await fetch(`${API}/${id}`, {
       method: "GET",
     });
     const data = await res.json();
-    console.log(data);
+    //display all students info in UI
+    mapAllStudents(data);
   } catch (error) {
     console.log(error);
   }
@@ -37,7 +32,8 @@ async function addNewStudentData(payload) {
       },
     });
     const data = await res.json();
-    console.log(data);
+    //display all students info
+    appendNewStudent(data);
   } catch (error) {
     console.log(error);
   }
@@ -55,7 +51,9 @@ async function editStudent(payload, id) {
       },
     });
     const data = await res.json();
-    console.log(data);
+    if (data) {
+      location.reload();
+    }
   } catch (error) {
     console.log(error);
   }
@@ -63,7 +61,7 @@ async function editStudent(payload, id) {
 
 //DELETE API (To delete a data) //DELETE
 
-async function deleteStudent(id) {
+async function deleteStudent(id, parent) {
   try {
     const res = await fetch(`${API}/${id}`, {
       method: "DELETE",
@@ -72,15 +70,134 @@ async function deleteStudent(id) {
       },
     });
     const data = await res.json();
-    console.log(data);
+    if (data) {
+      parent.remove();
+    } else {
+      console.log("error occured");
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
-//executing helper functions
+//Getting All students information
+getStudentsData("");
 
-//getStudentsData("");
-//addNewStudentData(dummyData);
-//editStudent(changeName, "6");
-//deleteStudent("6");
+//Controllers
+
+//creating a new student info card
+function appendNewStudent(student) {
+  const mainDiv = document.createElement("div");
+  mainDiv.className = "card";
+  mainDiv.innerHTML += `
+  <h2>${student.name}</h2>
+  <p>Batch <span id="batch-value">${student.batch}</span></p>
+  <p>Age <span id="age-value">${student.age}</span></p>
+  <div class="action-btn-group">
+  <button data-id=${student.id} id="edit-btn" class="btn">Edit</button>
+  <button data-id=${student.id} id="del-btn" class="btn">Delete</button>
+  </div>
+  `;
+  studContainer.append(mainDiv);
+}
+
+//Form creation
+studentForm.innerHTML += `
+<form>
+<h2>Student Form</h2>
+<input
+  type="text"
+  name="name"
+  required
+  value=""
+  placeholder="Enter Student Name"
+  class="input-text"
+  id="input-name"
+/>
+
+<input
+  type="text"
+  name="batch"
+  required
+  value=""
+  placeholder="Enter Student Batch"
+  class="input-text"
+  id="input-batch"
+/>
+
+<input
+  type="number"
+  name="age"
+  required
+  value=""
+  placeholder="Enter Student Age"
+  class="input-text"
+  id="input-age"
+/>
+<button type="submit" id="add-btn" class="btn">Add Student</button>
+<button type="submit" id="update-btn" class="btn">Update Student</button>
+</form>
+`;
+
+const input_name = document.querySelector("#input-name");
+const input_batch = document.querySelector("#input-batch");
+const input_age = document.querySelector("#input-age");
+const updateBtn = document.querySelector("#update-btn");
+const addBtn = document.querySelector("#add-btn");
+updateBtn.style.display = "none";
+
+// map all students recived from api
+function mapAllStudents(students) {
+  students.map((value) => {
+    appendNewStudent(value);
+  });
+}
+
+function clearForm() {
+  input_name.value = "";
+  input_age.value = "";
+  input_batch.value = "";
+}
+
+function getUserInputValues() {
+  return {
+    name: input_name.value,
+    batch: input_batch.value,
+    age: input_age.value,
+  };
+}
+
+function populateForm(parentNode) {
+  input_name.value = parentNode.querySelector("h2").innerText;
+  input_batch.value = parentNode.querySelector("#batch-value").innerText;
+  input_age.value = parentNode.querySelector("#age-value").innerText;
+  addBtn.style.display = "none";
+  updateBtn.style.display = "block";
+}
+
+studentForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.id == "add-btn") {
+    //get user input data
+    const payload = getUserInputValues();
+    console.log(payload);
+    addNewStudentData(payload);
+    clearForm();
+  }
+  if (e.target.id == "update-btn") {
+    const payload = getUserInputValues();
+    editStudent(payload, editId);
+  }
+});
+
+studContainer.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  const parentNode = event.target.parentNode.parentNode;
+  if (event.target.id == "del-btn") {
+    deleteStudent(id, parentNode);
+  }
+  if (event.target.id == "edit-btn") {
+    populateForm(parentNode);
+    editId = id;
+  }
+});
