3fbc53f73708867354165f181972963eb26cdec0
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-03-24 16:31:41 +0530

    React day-2 "events, state and map of components"

--- FSD-REACT/src/App.jsx
+++ FSD-REACT/src/App.jsx
@@ -1,33 +1,118 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  //state management using states
+  const [count, setCount] = useState(0);
   return (
     <>
-      <Profile name="Sanjay" batch="53" />
-      <Profile name="Vignesh" batch="54" />
-      <Profile name="Chandra" batch="55" />
+      <ProductCounter count={count} />
+      <AllPriceCard setCount={setCount} count={count} />
     </>
   );
 }
-
 export default App;
-//props = {} => {name : "sanjay", batch="53"}
-
-function Profile({ name, batch }) {
-  //jsx will be returned
+//This components hols and renders all product components
+function AllPriceCard({ setCount, count }) {
+  const data = [
+    {
+      type: "PRO",
+      price: {
+        value: 500,
+        style: "abled",
+      },
+      storage: {
+        value: 100,
+        style: "abled",
+      },
+      access: {
+        value: 100,
+        style: "abled",
+      },
+    },
+    {
+      type: "FREE",
+      price: {
+        value: 0,
+        style: "abled",
+      },
+      storage: {
+        value: 0,
+        style: "disabled",
+      },
+      access: {
+        value: 1,
+        style: "disabled",
+      },
+    },
+    {
+      type: "Standard",
+      price: {
+        value: 100,
+        style: "abled",
+      },
+      storage: {
+        value: 10,
+        style: "abled",
+      },
+      access: {
+        value: 1,
+        style: "disabled",
+      },
+    },
+  ];
   return (
-    <div className="profile-container">
-      <img
-        src="https://www.animaker.com/hub/wp-content/uploads/2023/03/Mickey_Mouse_Disney_1.webp"
-        alt="#"
-      />
-      <h3>{name}</h3>
-      <h3>{batch}</h3>
+    <div className="all-cards">
+      {/* <PriceCard model={free}/> */}
+      {data.map((val, idx) => (
+        <PriceCard model={val} key={idx} setCount={setCount} count={count} />
+      ))}
     </div>
   );
 }
 
+function PriceCard({ model, setCount, count }) {
+  const [show, setShow] = useState(true);
+
+  function handleBuy() {
+    setShow(!show);
+    setCount(count + 1);
+  }
+
+  function handleRemove() {
+    setShow(!show);
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="price-card">
+      <p className="type">{model.type}</p>
+      <p className={model.price.style}>Price : {model.price.value}$</p>
+      <p className={model.storage.style}>
+        {" "}
+        <span>❌</span>Storage : {model.storage.value}gb
+      </p>
+      <p className={model.access.style}>
+        <span>✅</span> Access : {model.access.value}
+      </p>
+      {/* conditional rendering */}
+      {show ? (
+        <button onClick={handleBuy}>Buy</button>
+      ) : (
+        <button onClick={handleRemove}>Remove</button>
+      )}
+    </div>
+  );
+}
+
+function ProductCounter({ count }) {
+  return (
+    <div>
+      <h1>CART : {count}</h1>
+    </div>
+  );
+}
 //React.createElement("h1", { className: "head", id: "h-1" }, `JSX ${name}`);
 
 // JSX and HOW it works
